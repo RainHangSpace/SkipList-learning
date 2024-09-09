57801e551724bc95e177e324c79d1a212c4ea48a
rainCold <1017543088@qq.com>
2024-09-09 17:07:00 +0800

    增加了跳表的搜索、插入和展示功能

--- main.cpp
+++ main.cpp
@@ -8,7 +8,33 @@
 
 #include <iostream>
 #include "skiplist.h"
-
+#include <string>
+#include <ctime>
 int main() {
-    SkipList<int, int> skipList(6);
+    std::srand(std::time(0)); // 设置随机种子
+    //测试1：
+    // SkipList<int, int> skipList1(6);
+    // SkipList<std::string, int> skipList2(6);
+    // Node<int, int> node1(1, 1, 0);
+    // Node<int, std::string> node2(2, "b", 0);
+    // Node<int, int>* node1_p = &node1;
+    // Node<int, std::string>* node2_p = &node2; 
+    // Node<std::string, int>* node3_p = skipList2.create_node("zhangyuhan", 363, 0);
+    // std::cout << node1.get_key() << ' ' << node1_p->get_value() << '\n';
+    // std::cout << node2.get_key() << ' ' << node2_p->get_value() << '\n';
+    // std::cout << node3_p->get_key() << ' ' << node3_p->get_value() << '\n';
+    
+    
+    //测试2：
+    SkipList<std::string, int> SkipList1(7);
+    SkipList1.insert_element("zhangyuhan", 363);
+    SkipList1.display_list();
+    std::cout << '\n';
+    SkipList1.insert_element("zhangyuhan", 363);
+    SkipList1.insert_element("wangyumeng", 636);
+    SkipList1.display_list();
+
+    if (SkipList1.search_element("wangyumeng"))
+        std::cout << "Search Success";
+
 }
--- skiplist.h
+++ skiplist.h
@@ -12,10 +12,14 @@
 #include <cmath>
 #include <cstring>
 
+//class declaration for SkipList
+template<typename K, typename V>
+class SkipList;
 
 //Class template to implement node
 template<typename K, typename V>
 class Node {
+    friend class SkipList<K, V>;
 public:
     Node() {}
 
@@ -84,7 +88,7 @@ public:
     ~SkipList();
     size_t get_random_level();
     Node<K, V>* create_node(K, V, size_t);
-    int insert_element(K, V);
+    bool insert_element(K, V);
     void display_list();
     bool search_element(K);
     void delete_element(K);
@@ -101,7 +105,7 @@ private:
     size_t _skip_list_level;
 
     //head node for skiplist
-    Node<K, V> *_head;
+    Node<K, V> *_header;
 
     //number of elements
     size_t _element_count;
@@ -117,23 +121,25 @@ public:
 
 template<typename K, typename V>
 SkipList<K, V>::SkipList(size_t max_level) 
-    :_max_level{max_level}, _skip_list_level{0}, _head{nullptr},
+    :_max_level{max_level}, _skip_list_level{0}, _header{nullptr},
     _element_count{0}
 {
-    K k;
-    V v;
+    K k;//default key
+    V v;//default value
     try {
-        _head = new Node<K, V>(k, v, _max_level);
+        _header = new Node<K, V>(k, v, _max_level);
     } catch(std::bad_alloc& e) {
         std::cerr << "Bad allocation for SkipList: " << e.what() << std::endl;
-        _head = nullptr;
+        _header = nullptr;
         throw;
     }
 }
+
 template<typename K, typename V>
 SkipList<K, V>::~SkipList() {
     ;
 }
+
 template<typename K, typename V>
 size_t SkipList<K, V>::get_random_level() {
     size_t k = 1;
@@ -146,6 +152,100 @@ size_t SkipList<K, V>::get_random_level() {
     return k;
 }
 
+template<typename K, typename V>
+Node<K, V>* SkipList<K, V>::create_node(K k, V v, size_t level) {
+    Node<K, V>* p = nullptr;
+    try {
+        p = new Node<K, V>(k, v, level);
+    }
+    catch(const std::bad_alloc& e) {
+        std::cerr << "SkipList::create_node error! " << e.what() << '\n';
+        p = nullptr;
+        throw;
+    }
+    return p;
+}
+
+//查找
+template<typename K, typename V>
+bool SkipList<K, V>::search_element(K target) {
+    Node<K, V>* current = _header;
+    //注意：用size_t进行索引的时候 不能以大于等于零作判断条件
+    for (size_t i = _skip_list_level + 1; i > 0; --i) {
+        while(current->_forward[i - 1] && current->_forward[i - 1]->get_key() < target) {
+            current = current->_forward[i - 1];
+        }
+    }
+    current = current->_forward[0];//一定会下沉到最底层
+    if (!current || current->get_key() > target)
+        return false;
+    return true;
+}
+//插入
+template<typename K, typename V>
+bool SkipList<K, V>::insert_element(K k, V v) {
+    
+    //用current去查找需要插入的位置
+    Node<K, V>* current = _header;
+    // current = current->_forward[0];
+    // current = _header;
+    // std::cout << "current:" << _header << '\n';
+    //用数组记录插入节点时，需要更新的前驱结点
+    Node<K, V>* pre_node_to_update[_max_level + 1];
+
+    //注意：用size_t进行索引的时候 不能以大于等于零作判断条件
+    for (size_t i = _skip_list_level + 1; i > 0; --i) {
+        // std::cout << "i: " << i << '\n';
+        while (current->_forward[i - 1] && current->_forward[i - 1]->get_key() < k) {
+            // std::cout << i << " ";
+            current = current->_forward[i - 1];
+        }
+        // std::cout << "i: " << i << '\n';
+        //当退出上面的while时，就说明找到了要插入的位置，即current的后面，所以要存储到数组中，等待更新
+        pre_node_to_update[i - 1] = current;
+        // std::cout << "b\n";        
+    }
+    // std::cout << "c\n";
+    current = current->_forward[0];
+    // std::cout << "d: " << current << '\n';
+    if (current && current->get_key() == k)
+        return false;//如果不用插入 返回false
+    
+    size_t level = get_random_level();//随机生成level
+    // std::cout << "d: " << level << '\n';    
+    if (level > _skip_list_level) {
+        for (size_t i = _skip_list_level + 1; i <= level; ++i) {
+            pre_node_to_update[i] = _header;
+        }
+        //记得更新_skip_list_level
+        _skip_list_level = level;
+    }
+    // std::cout << "_skip_list_level = " << _skip_list_level << '\n';
+    Node<K, V>* node_p = create_node(k, v, level);
+    for (size_t i = 0; i <= level; ++i) {
+        node_p->_forward[i] = pre_node_to_update[i]->_forward[i];
+        pre_node_to_update[i]->_forward[i] = node_p;
+    }
+    //记得更新元素数量
+    ++_element_count;
+    return true;
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::display_list() {
+    Node<K, V>* current = nullptr;
+    //注意：用size_t进行索引的时候 不能以大于等于零作判断条件
+    for (size_t i = _skip_list_level + 1; i > 0; --i) {
+        std::cout << "level" << i - 1 << ": ";
+        current = _header->_forward[i - 1];
+        while (current) {
+            std::cout << "(" << current->get_key() << ", " << current->get_value() << ") ";
+            current = current->_forward[i - 1];
+        }
+        std::cout << '\n';
+    }
+    std::cout << std::endl;
+}
 
 
 
